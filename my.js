19c9b6e2b6c6bc16788f7660a806fc1f77d75de3
linhhm24 <linh.hm24@gmail.com>
2020-02-24 22:32:49 +0700

    Add file

---
+++ my.js
@@ -0,0 +1,7 @@
+let input_number=null;
+
+do {
+    input_number= parseInt(prompt('nhap vao so tu 1 den 10'));
+} while (input_number <1 || input_number>10);
+
+alert('So vua nhap la '+input_number);
